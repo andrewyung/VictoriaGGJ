a2100c2cb1fc112404409d23a7ce8d355283bf6b
andrewyung <andrewyung@uvic.ca>
2017-01-20 19:25:01 -0800

    added the rest

---
+++ Assets/MagneticObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagneticObject : MonoBehaviour {
+
+    [SerializeField]
+    private float forceAmount;
+
+    private Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // get normalized direction of player
+        Vector3 playerDirectionNormalized = (collider.transform.position - transform.position);
+
+        //collider.GetComponent<Rigidbody>().addFor
+    }
+}
